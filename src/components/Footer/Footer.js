dc46e251e938afb0555f26bb224d8bdfb44b4f3a
mayankmohak <mayank8199@gmail.com>
2021-06-11 01:53:23 +0530

    Adding footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Style Sheet
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer>
+      <hr />
+      <div className="black"></div>
+      <p>Made with ❤️ by OpenSource <a href="https://github.com/rockingrohit9639/project-zone/graphs/contributors" target="_blank">Contributors</a>.</p>
+    </footer>
+  );
+}
+
+export default Footer;
