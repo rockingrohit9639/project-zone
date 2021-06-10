5dbd0fb87edbe1fbffabfb446cbf18dfba4695b1
naveen8801 <naveensharma10d@gmail.com>
2021-06-10 20:53:32 +0530

    Share Project feature added

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,27 +1,40 @@
-import React from 'react';
-import "./Project.css";
+import React, { useState } from 'react';
+import './Project.css';
+import share_logo from './../../assets/share.svg';
+import ShareProject from '../ShareProject/ShareProject';
 
-function Project({ title, desc, skills, level, style })
-{
-
-    return (
-        <div className="project" style={style && style}>
-            <h3 className="title"> {title}</h3>
-            <p className="description">{desc}</p>
-
-            <div className="level">
-                <label>Level:- </label>
-                <p>{level}</p>
+function Project({ title, desc, skills, level, style }) {
+  const [shareopen, setshareopen] = useState(false);
+  const shareButtonHandler = () => {
+    setshareopen(!shareopen);
+  };
+  return (
+    <div className="project" style={style && style}>
+      <div className="title-flexbox">
+        <h3 className="title"> {title}</h3>
+        <img
+          src={share_logo}
+          className="share-img"
+          alt="share-logo"
+          onClick={shareButtonHandler}
+        />
+      </div>
+      {shareopen ? <ShareProject title={title} description={desc} /> : null }
+      <p className="description">{desc}</p>
+      <div className="level">
+        <label>Level:- </label>
+        <p>{level}</p>
+      </div>
+      <div className="skills">
+        {skills &&
+          skills.map((skill, ind) => (
+            <div key={ind} className="skill">
+              {skill}{' '}
             </div>
-
-            <div className="skills">
-                {
-                    skills && skills.map((skill, ind) => (<div key={ind} className="skill">{ skill} </div>))
-                }
-
-            </div>
-        </div>
-    )
+          ))}
+      </div>
+    </div>
+  );
 }
 
-export default Project
+export default Project;
---
+++ src/components/ShareProject/ShareProject.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './ShareProject.css';
+import {
+  EmailIcon,
+  EmailShareButton,
+  LinkedinShareButton,
+  RedditIcon,
+  RedditShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+  LinkedinIcon,
+  FacebookShareButton,
+  FacebookIcon,
+} from 'react-share';
+
+function ShareProject(props) {
+  const url = `https://project-zone.netlify.app/`;
+  const quote = `Hello,
+I found an awesome project for you i.e ' ${props.title} '
+${props.description}
+
+Have a look at it and enjoy coding.
+For more interesting project visit `;
+
+  return (
+    <div className="share-conatiner">
+      <div>
+        <TwitterShareButton url={url} title={quote}>
+          <TwitterIcon size={35} round />
+        </TwitterShareButton>
+      </div>
+      <div>
+        <WhatsappShareButton url={url} title={quote}>
+          <WhatsappIcon size={35} round />
+        </WhatsappShareButton>
+      </div>
+      <div>
+        <RedditShareButton url={url} title={quote}>
+          <RedditIcon  size={35} round />
+        </RedditShareButton>
+      </div>
+      <div>
+        <FacebookShareButton url={url} title={quote}>
+          <FacebookIcon size={35} round />
+        </FacebookShareButton>
+      </div>
+      <div>
+        <LinkedinShareButton url={url} summary={quote}>
+          <LinkedinIcon size={35} round />
+        </LinkedinShareButton>
+      </div>
+    </div>
+  );
+}
+
+export default ShareProject;
