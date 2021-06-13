affa4fc3ecaeeeed17f6532de7b8c90b107cff7d
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-06-13 18:00:05 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -7,14 +7,16 @@ const colors = require('colors');
 dotenv.config({ path: 'config/config.env' });
 const app = express(); // create express app
 app.use(morgan('tiny'));
+app.use(cors())
 app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 3001;
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>
+{
   res.send('Hellow world Express Server 3001');
 });
 
 // start express server on port 3001
 app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`.green.bold)
+  console.log(`Server running on port http://localhost:${PORT}/`.green.bold)
 );
