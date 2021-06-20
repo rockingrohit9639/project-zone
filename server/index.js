24e8e6496ab2b19bdf6ba472788deacd1716d881
naveen8801 <naveensharma10d@gmail.com>
2021-06-20 10:43:31 +0530

    Signin, Signup & Protected route added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const bodyParser = require('body-parser');
 
 dotenv.config({ path: 'config/config.env' });
 const PORT = process.env.PORT || 3001;
@@ -10,16 +11,15 @@ const PORT = process.env.PORT || 3001;
 const app = express(); // create express app
 
 app.use(morgan('tiny'));
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json({ extended: false }));
 
-
-require("./db/connection");
+require('./db/connection');
 
 app.use(require('./routes/projectRoutes'));
-
-
-
+app.use(require('./routes/authRoutes'));
+app.use(require('./routes/ProtectedRoutes'));
 
 // start express server on port 3001
 app.listen(PORT, () =>
