eabd014b5a9d2c3062b70c51b50e63bace0549b4
ShwetKhatri2001 <19dcs001@lnmiit.ac.in>
2021-06-25 03:12:22 +0530

    changed card design and home projects layout

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Project.css';
 import share_logo from './../../assets/share.svg';
+import cardimg from './../../assets/cardtop.svg';
 import ShareProject from '../ShareProject/ShareProject';
 import RatingCard from '../RatingCard/RatingCard';
 import { Link } from 'react-router-dom';
@@ -27,42 +28,50 @@ function Project({ title, desc, skills, level, style, rating }) {
   };
   return (
     <div className='project' style={style && style}>
-      <div className='title-flexbox'>
-        <h3 className='title'> {title}</h3>
-        <img
-          src={share_logo}
-          className='share-img'
-          alt='share-logo'
-          onClick={shareButtonHandler}
-        />
+      <div className="cardimg-box">
+        <img 
+            src={cardimg}
+            className="cardimg"
+            alt="card-image"
+          />
       </div>
-      {shareopen ? <ShareProject title={title} description={desc} /> : null}
-      <div className='descr'>
-        {desc.trim() == '' ? null : (
-          <p className='description'>{desc.slice(0, 60) + '...'}</p>
-        )}
-        <Link to='/projectdetails'>
-          <h5 className='read-more' onClick={ReadMeHandler}>
-            Read more
-          </h5>
-        </Link>
-      </div>
-      <div className='level'>
-        <label>Level:- </label>
-        <p>{level}</p>
-      </div>
+      <div className="card-content">
+        <div className='title-flexbox'>
+          <h3 className='title'> {title}</h3>
+          <img
+            src={share_logo}
+            className='share-img'
+            alt='share-logo'
+            onClick={shareButtonHandler}
+          />
+          {shareopen ? <ShareProject title={title} description={desc} /> : null}
+        </div>
+        <div className='descr'>
+          {desc.trim() == '' ? null : (
+            <p className='description'>{desc.slice(0, 120) + '...'}</p>
+          )}
+          <Link to='/projectdetails'>
+            <h5 className='read-more' onClick={ReadMeHandler}>
+              Read more
+            </h5>
+          </Link>
+        </div>
+        <div className='level'>
+          <label>Level - </label>
+          <p>{level}</p>
+        </div>
 
-      <div className='skills'>
-        {skills &&
-          skills.map((skill, ind) => (
-            <div key={ind} style={getSkillColor(skill)} className='skill'>
-              {skill}
-            </div>
-          ))}
-      </div>
-
-      <div>
-        <RatingCard rating={rating} />
+        <div className='skills'>
+          {skills &&
+            skills.map((skill, ind) => (
+              <div key={ind} style={getSkillColor(skill)} className='skill'>
+                {skill}
+              </div>
+            ))}
+        </div>
+        <div>
+          <RatingCard rating={rating} />
+        </div>
       </div>
     </div>
   );
