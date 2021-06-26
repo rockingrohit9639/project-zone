de59e66c92f19c063f3374eec54f923eb603e5b6
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-06-26 16:47:44 +0530

    minor changes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -53,7 +53,7 @@ const Profile = () => {
              </div>
              <div className="content_right">
                   <div className="email_descr">
-                     <h3>Software Engineer</h3>
+                     <h3> testuser@projectzone.com </h3>
                      <p>This is the description.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                   </div>
                   <div className="projects_badges">
