667dbc87d76d215df314d9179dc0385e53a54d50
Rohan Modi <rohanmodi790@gmail.com>
2021-06-27 19:24:25 +0530

    Updated Particle Background Animation

--- src/components/TrendingProjects/TrendingProjects.js
+++ src/components/TrendingProjects/TrendingProjects.js
@@ -2,48 +2,59 @@ import React from 'react';
 import './TrendingProjects.css';
 import star from './../../assets/star.svg';
 import Project from '../Project/Project';
+import ParticlesBg from 'particles-bg';
 
 function TrendingProjects(props) {
-  return (
-    <div className="conatiner">
-      <img src={star} className="star-logo" alt="star-logo" />
-      <h2 className="best_project--title">Trending Projects</h2>
-      <div className="flex-box">
-        <Project
-          className="project"
-          title="E-Commerce Website"
-          desc=""
-          skills={["mongodb", "express", "react", "nodejs"]}
-          level="Beginner"
-          rating={5}
+    return ( <
+        div className = "conatiner" >
+        <
+        img src = { star }
+        className = "star-logo"
+        alt = "star-logo" / >
+        <
+        ParticlesBg type = "thick"
+        bg = { true }
         />
-        <Project
-          className="project"
-          title="Online Distance Education System"
-          desc=""
-          skills={["mongodb", "express", "react", "nodejs"]}
-          level="Advance"
-          rating={5}
-        />
-        <Project
-          className="project"
-          title="E-Learning Portal"
-          desc=""
-          skills={["mongodb", "express", "react", "nodejs"]}
-          level="Advanced"
-          rating={4}
-        />
-        <Project
-          className="project"
-          title="Virtual Queue Management System"
-          desc=""
-          skills={["mongodb", "express", "react", "nodejs"]}
-          level="Advanced"
-          rating={4}
-        />
-      </div>
-    </div>
-  );
+
+        <
+        h2 className = "best_project--title" > Trending Projects < /h2> <
+        div className = "flex-box" >
+        <
+        Project className = "project"
+        title = "E-Commerce Website"
+        desc = ""
+        skills = {
+            ["mongodb", "express", "react", "nodejs"] }
+        level = "Beginner"
+        rating = { 5 }
+        /> <
+        Project className = "project"
+        title = "Online Distance Education System"
+        desc = ""
+        skills = {
+            ["mongodb", "express", "react", "nodejs"] }
+        level = "Advance"
+        rating = { 5 }
+        /> <
+        Project className = "project"
+        title = "E-Learning Portal"
+        desc = ""
+        skills = {
+            ["mongodb", "express", "react", "nodejs"] }
+        level = "Advanced"
+        rating = { 4 }
+        /> <
+        Project className = "project"
+        title = "Virtual Queue Management System"
+        desc = ""
+        skills = {
+            ["mongodb", "express", "react", "nodejs"] }
+        level = "Advanced"
+        rating = { 4 }
+        /> <
+        /div> <
+        /div>
+    );
 }
 
 export default TrendingProjects;
