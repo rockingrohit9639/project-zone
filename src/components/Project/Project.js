0c41307edadb9158052a13a2c502a9db05c0cea3
naveen8801 <naveensharma10d@gmail.com>
2021-07-04 21:03:16 +0530

    Home page made dynamic

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,25 +1,22 @@
-import React, { useState } from 'react';
-import './Project.css';
-import share_logo from './../../assets/share.svg';
-import cardimg from './../../assets/cardtop.svg';
-import ShareProject from '../ShareProject/ShareProject';
-import RatingCard from '../RatingCard/RatingCard';
-import { Link } from 'react-router-dom';
-import { useDataLayerValues } from '../../datalayer';
-import { getSkillColor } from '../../utils';
+import React, { useState } from "react";
+import "./Project.css";
+import share_logo from "./../../assets/share.svg";
+import cardimg from "./../../assets/cardtop.svg";
+import ShareProject from "../ShareProject/ShareProject";
+import RatingCard from "../RatingCard/RatingCard";
+import { Link } from "react-router-dom";
+import { useDataLayerValues } from "../../datalayer";
+import { getSkillColor } from "../../utils";
 
-function Project({ title, desc, skills, level, style, rating })
-{
+function Project({ title, desc, skills, level, style, rating, likes }) {
   const [shareopen, setshareopen] = useState(false);
   const [{ ProjectDetails }, dispatch] = useDataLayerValues();
-  const shareButtonHandler = () =>
-  {
+  const shareButtonHandler = () => {
     setshareopen(!shareopen);
   };
-  const ReadMeHandler = () =>
-  {
+  const ReadMeHandler = () => {
     dispatch({
-      type: 'SET_PROJECT_DETAILS',
+      type: "SET_PROJECT_DETAILS",
       ProjectDetails: {
         title: title,
         descr: desc,
@@ -30,48 +27,47 @@ function Project({ title, desc, skills, level, style, rating })
     });
   };
   return (
-    <div className='project' style={style && style}>
+    <div className="project" style={style && style}>
       <div className="cardimg-box">
-        <img
-          src={cardimg}
-          className="cardimg"
-          alt="card-image"
-        />
+        <img src={cardimg} className="cardimg" alt="card-image" />
       </div>
       <div className="card-content">
-        <div className='title-flexbox'>
-          <h3 className='title'> {title}</h3>
+        <div className="title-flexbox">
+          <h3 className="title"> {title}</h3>
           <img
             src={share_logo}
-            className='share-img'
-            alt='share-logo'
+            className="share-img"
+            alt="share-logo"
             onClick={shareButtonHandler}
           />
           {shareopen ? <ShareProject title={title} description={desc} /> : null}
         </div>
-        <div className='descr'>
-          {desc.trim() == '' ? null : (
-            <p className='description'>{desc.slice(0, 100) + '...'}</p>
+        <div className="descr">
+          {desc.trim() == "" ? null : (
+            <p className="description">{desc.slice(0, 100) + "..."}</p>
           )}
-          <Link to='/projectdetails'>
-            <h5 className='read-more' onClick={ReadMeHandler}>
+          <Link to="/projectdetails">
+            <h5 className="read-more" onClick={ReadMeHandler}>
               Read more
             </h5>
           </Link>
         </div>
 
-        <div className='level'>
+        <div className="level">
           <label>Level - </label>
           <p>{level}</p>
         </div>
 
         <div className="likebox">
-          <i className="far fa-heart" aria-hidden="true" > <span>35 Likes</span></i>
+          <i className="far fa-heart" aria-hidden="true">
+            {" "}
+            <span>{likes}</span>
+          </i>
         </div>
-        <div className='skills'>
+        <div className="skills">
           {skills &&
             skills.map((skill, ind) => (
-              <div key={ind} style={getSkillColor(skill)} className='skill'>
+              <div key={ind} style={getSkillColor(skill)} className="skill">
                 {skill}
               </div>
             ))}
